50a44f454f28105ef39ab9d732df1f3b2926ae77
Vladimir Frolov <vova@frolov.pp.ru>
2024-01-03 14:24:56 +0300

    edit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 
+#include "adam.h"
 #include "checker.h"
 #include "spectrum.h"
 #include "imageutils.h"
@@ -61,8 +62,8 @@ struct TestData
     f3_data.resize(a_size);
     ref_data.resize(a_size);
     
-    double x = 0.0;
-    double step = 10/double(a_size);
+    float x = 0.0;
+    float step = 10/float(a_size);
     for(int i=0;i<a_size;i++,x+=step)
     {
       x_data  [i] = x;
@@ -73,18 +74,18 @@ struct TestData
     }
   }
 
-  std::vector<double> x_data; 
-  std::vector<double> f1_data;
-  std::vector<double> f2_data;
-  std::vector<double> f3_data;
-  std::vector<double> ref_data; // 
+  std::vector<float> x_data; 
+  std::vector<float> f1_data;
+  std::vector<float> f2_data;
+  std::vector<float> f3_data;
+  std::vector<float> ref_data; // 
 };
 
-double TestDataLoss(double* __restrict__ f1_val, 
-                    double* __restrict__ f2_val,
-                    double* __restrict__ f3_val,
-                    double* __restrict__ ref_val,
-                    double* __restrict__ x_data, size_t n)
+double TestDataLoss(float* __restrict__ f1_val, 
+                    float* __restrict__ f2_val,
+                    float* __restrict__ f3_val,
+                    float* __restrict__ ref_val,
+                    float* __restrict__ x_data, size_t n)
 {
   double lossVal = 0.0;
   for(size_t i=0;i<n;i++) 
@@ -96,149 +97,39 @@ double TestDataLoss(double* __restrict__ f1_val,
   return lossVal; 
 }
 
-struct AdamOptimizer
-{
-  void Init(int a_size)
-  {
-    momentum.resize(a_size);
-    grad.resize(a_size);
-    m_GSquare.resize(a_size);
-    std::fill(momentum.begin(), momentum.end(), 0.0);
-    std::fill(grad.begin(), grad.end(), 0.0);
-    std::fill(m_GSquare.begin(), m_GSquare.end(), 0.0);
-  }
-  
-  void UpdateState(double* a_state, int iter)
-  {
-    int factorGamma    = iter/100 + 1;
-    const double alpha = 0.5;
-    const double beta  = 0.25;
-    const double gamma = 0.25/factorGamma;
-    
-    // Adam: m[i] = b*mPrev[i] + (1-b)*gradF[i], 
-    // GSquare[i] = GSquarePrev[i]*a + (1.0f-a)*grad[i]*grad[i])
-    for(size_t i=0;i<grad.size();i++)
-    {
-      momentum [i] = momentum[i]*beta + grad[i]*(1.0-beta);
-      m_GSquare[i] = 2.0*(m_GSquare[i]*alpha + (grad[i]*grad[i])*(1.0-alpha)); // does not works without 2.0
-    }
-
-    //xNext[i] = x[i] - gamma/(sqrt(GSquare[i] + epsilon)); 
-    for (int i=0;i<grad.size();i++) 
-      a_state[i] -= (gamma*momentum[i]/(std::sqrt(m_GSquare[i] + epsilon)));  
-  }
-
-  std::vector<double> momentum; 
-  std::vector<double> grad;
-  std::vector<double> m_GSquare;
-  double epsilon = 1e-20f;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*void testXYZ()
-{
-  auto rects    = GetCheckerRects();
-  auto colorLDR = LoadAveragedCheckerLDRData("/home/frol/PROG/HydraRepos/HydraCore3/z_checker.bmp", rects); 
-  
-  int  channels = 0;
-  auto colorHDR = LoadAveragedSpectrumFromImage3d1f("/home/frol/PROG/HydraRepos/HydraCore3/z_checker.image3d1f", rects, &channels); 
-
-  auto spdLight = LoadAndResampleSpectrum("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/data/Spectral_data/Lights/FalconEyesStudioLEDCOB120BW.spd",  channels); 
-  auto spdMats  = LoadAndResampleAllCheckerSpectrum("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/data/Spectral_data/DatacolorSpyderCheckr24_card2", channels);
-  
-  Spectrum curveX, curveY, curveZ;
-  {
-    curveX.wavelengths = Get_CIE_lambda();
-    curveX.values      = Get_CIE_X();
-  
-    curveY.wavelengths = Get_CIE_lambda();
-    curveY.values      = Get_CIE_Y();
-  
-    curveZ.wavelengths = Get_CIE_lambda();
-    curveZ.values      = Get_CIE_Z();
-  }
-
-  auto curveX1 = curveX.Resample(channels);
-  auto curveY1 = curveY.Resample(channels);
-  auto curveZ1 = curveZ.Resample(channels);
-
-  int a = 2;
-  std::cout << "curveX1.size() = " << curveX1.size() << std::endl;
-
-  const float CIE_Y_integral = 106.856895f;
-
-  // test spectrum image to RGB image
-  //
-  std::vector<float3> colorLDR2(rects.size());
-  for(int rectId = 0; rectId < int(rects.size()); rectId++) {
-    const float* colorSpec = colorHDR.data() + rectId*channels;
-    float3 colorAccum(0,0,0);
-    for(int c = 0; c < channels; c++) {
-      float val = colorSpec[c];
-      float lambda = LAMBDA_MIN + (float(c+0.5f)/float(channels))*(LAMBDA_MAX - LAMBDA_MIN);
-      
-      float3 xyz = SpectrumToXYZ(val, lambda, LAMBDA_MIN, LAMBDA_MAX,
-                                 curveX.values.data(), curveY.values.data(), curveZ.values.data());
-      
-      //float3 xyz = {curveX1[c]*(val/pdf), curveY1[c]*(val/pdf), curveZ1[c]*(val/pdf)};
-      float3 rgb = XYZToRGB(xyz);
-      //if(rectId == 12) { 
-      //  std::cout << "val,X = " << val << ", " << curveX1[c] << std::endl;
-      //  //std::cout << "xyz = " << xyz.x << " " << xyz.y << " " << xyz.z << std::endl;
-      //  //std::cout << "rgb = " << rgb.x << " " << rgb.y << " " << rgb.z << std::endl;
-      //}
-      colorAccum += rgb;
-    }
-    colorLDR2[rectId] = colorAccum*255.0f*(CIE_Y_integral/float(channels)); // * 100000.0f
-  }
-
-  for(size_t rectId = 0; rectId < colorLDR.size(); rectId++) {
-    std::cout << "from_spd" << rectId << ":\t(" << colorLDR2[rectId].x << ", " << colorLDR2[rectId].y << ", " << colorLDR2[rectId].z << ")" << std::endl; 
-  }
-  
-  std::cout << std::endl;
-  for(size_t rectId = 0; rectId < colorLDR.size(); rectId++) {
-    std::cout << "from_bmp" << rectId << ":\t(" << int(colorLDR[rectId].x+0.5f) << ", " << int(colorLDR[rectId].y+0.5f) << ", " << int(colorLDR[rectId].z+0.5f) << ")" << std::endl; 
-    std::cout << "from_spd" << rectId << ":\t(" << int(colorLDR2[rectId].x+0.5f) << ", " << int(colorLDR2[rectId].y+0.5f) << ", " << int(colorLDR2[rectId].z+0.5f) << ")" << std::endl; 
-    std::cout << std::endl;
-  }
-  std::cout << "channelNum = " << channels << std::endl;
-}*/
 
 void testGaussians()
 {
 
   TestData data;
-  AdamOptimizer opt;
+  data.Init(100);
 
-  data.Init(100);  
-  opt.Init(100);
+  AdamOptimizer<float> opt(100);
+  std::vector<float> grad(100);
 
   auto initial_f1 = data.f1_data;
   
   for(int iter = 0; iter < 50; iter++) 
   {
-    std::fill(opt.grad.begin(), opt.grad.end(), 0.0);  
+    std::fill(grad.begin(), grad.end(), 0.0);  
 
-    double dloss = __enzyme_autodiff((void*)TestDataLoss,
-                                     enzyme_dup,   data.f1_data.data(), opt.grad.data(),
+    float dloss = __enzyme_autodiff((void*)TestDataLoss,
+                                     enzyme_dup,   data.f1_data.data(), grad.data(),
                                      enzyme_const, data.f2_data.data(),
                                      enzyme_const, data.f3_data.data(),
                                      enzyme_const, data.ref_data.data(),
                                      enzyme_const, data.x_data.data(),
-                                     enzyme_const, opt.grad.size());
+                                     enzyme_const, grad.size());
     
-    double lossVal = TestDataLoss(data.f1_data.data(),
+    float lossVal = TestDataLoss(data.f1_data.data(),
                                   data.f2_data.data(),
                                   data.f3_data.data(),
                                   data.ref_data.data(),
                                   data.x_data.data(),
-                                  opt.grad.size());                                     
+                                  grad.size());                                     
 
     //opt.UpdateGrad();
-    opt.UpdateState(data.f1_data.data(), iter);
+    opt.step(data.f1_data.data(), grad.data(), iter);
     
     //if((iter+1) % 10 == 0)
     std::cout << "iter = " << iter << ", loss = (" << lossVal << ")" << std::endl;
@@ -247,8 +138,8 @@ void testGaussians()
   std::ofstream fout2("data.csv");
   fout2 << "x;f1;f2;f3;f1xf2xf3;initial_f1;optimized_f1;" << std::endl;
 
-  double x = 0.0;
-  double step = 10/double(100);
+  float x = 0.0;
+  float step = 10/float(100);
   for(int i=0;i<100;i++,x+=step)
     fout2 << x << ";" << f1(x) << ";" << f2(x) << ";" << f3(x) << ";" << f1(x)*f2(x)*f3(x) << ";" << initial_f1[i] << ";" << data.f1_data[i] << ";" << std::endl;
   
@@ -341,7 +232,9 @@ float GoldenSectionCoeff2(float a, float b, float epsilon, FuncData data)
   return (a + b) / 2;
 }
 
-double EvalCurve1(double* camRGB, double* render, double* ref, size_t rectNum, int channelNum)
+double EvalCurve1(double* __restrict__ camRGB, 
+                  double* __restrict__ render, 
+                  double* __restrict__ ref, size_t rectNum, int channelNum)
 {
   float loss = 0.0f;
   for(size_t rectId = 0; rectId < rectNum; rectId++) {
@@ -354,20 +247,6 @@ double EvalCurve1(double* camRGB, double* render, double* ref, size_t rectNum, i
   }
   return loss;
 }
-
-/*
-auto spdLight = LoadAndResampleSpectrum("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/data/Spectral_data/Lights/FalconEyesStudioLEDCOB120BW.spd",  channels); 
-auto spdMats  = LoadAndResampleAllCheckerSpectrum("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/data/Spectral_data/DatacolorSpyderCheckr24_card2", channels);
-std::vector<float> renderCoeff(rects.size(), 1.0f);
-float loss = EvalRenderCoeff(renderCoeff.data(), spdLight.data(), spdMats.data(), avgSpec.data(), int(rects.size()), channels);
-std::cout << "initial loss = " << loss << std::endl;
-
-float minArgVal = GoldenSectionCoeff2(0.0f, 100.0f, 5e-6f, FuncData{spdLight.data(), spdMats.data(), avgSpec.data(), int(rects.size()), channels});
-float minLoss   = EvalRenderCoeff2(minArgVal, spdLight.data(), spdMats.data(), avgSpec.data(), int(rects.size()), channels);
-std::cout << "minLoss   = " << minLoss << std::endl;
-std::cout << "minArgVal = " << minArgVal << std::endl;
-*/
-
 
 void FitSingleCurvePerImage(std::vector<Rect> rects,
                             const char* refImagePath,
@@ -407,15 +286,15 @@ void FitSingleCurvePerImage(std::vector<Rect> rects,
   std::cout << "initialLoss = " << initialLossVal << std::endl;
   
   
-  AdamOptimizer opt;
-  opt.Init(curve.size());
+  AdamOptimizer<double> opt(curve.size());
+  std::vector<double> grad(curve.size());
   
   for(int iter = 0; iter < 500; iter++) 
   {
-    std::fill(opt.grad.begin(), opt.grad.end(), 0.0);  
+    std::fill(grad.begin(), grad.end(), 0.0);  
 
     double dloss = __enzyme_autodiff((void*)EvalCurve1,
-                                    enzyme_dup,   curve.data(), opt.grad.data(),
+                                    enzyme_dup,   curve.data(), grad.data(),
                                     enzyme_const, avgSpecD.data(),
                                     enzyme_const, colorD.data(),
                                     enzyme_const, rects.size(),
@@ -423,7 +302,7 @@ void FitSingleCurvePerImage(std::vector<Rect> rects,
     
     double lossVal = EvalCurve1(curve.data(), avgSpecD.data(), colorD.data(), rects.size(), channels);                                   
 
-    opt.UpdateState(curve.data(), iter);
+    opt.step(curve.data(), grad.data(), iter);
     for(size_t i=0;i<curve.size();i++)
       if(curve[i] < 0.0)
         curve[i] = 0.0;
@@ -470,6 +349,18 @@ void OptRGBCurvesForCamMulLight(const char* a_testDir, const char* a_testNumber)
                          spdImagePath.c_str(), "Canon60D_b_opt", 2);
 }
 
+/*
+auto spdLight = LoadAndResampleSpectrum("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/data/Spectral_data/Lights/FalconEyesStudioLEDCOB120BW.spd",  channels); 
+auto spdMats  = LoadAndResampleAllCheckerSpectrum("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/data/Spectral_data/DatacolorSpyderCheckr24_card2", channels);
+std::vector<float> renderCoeff(rects.size(), 1.0f);
+float loss = EvalRenderCoeff(renderCoeff.data(), spdLight.data(), spdMats.data(), avgSpec.data(), int(rects.size()), channels);
+std::cout << "initial loss = " << loss << std::endl;
+
+float minArgVal = GoldenSectionCoeff2(0.0f, 100.0f, 5e-6f, FuncData{spdLight.data(), spdMats.data(), avgSpec.data(), int(rects.size()), channels});
+float minLoss   = EvalRenderCoeff2(minArgVal, spdLight.data(), spdMats.data(), avgSpec.data(), int(rects.size()), channels);
+std::cout << "minLoss   = " << minLoss << std::endl;
+std::cout << "minArgVal = " << minArgVal << std::endl;
+*/
 
 int main(int argc, const char** argv) 
 {
@@ -477,8 +368,13 @@ int main(int argc, const char** argv)
   
   //OptRGBCurvesForCamMulLight("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/FalconEyesStudioLEDCOB120BW", "8459");
 
-  OptRGBCurvesForCamMulLight("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/LED_panel", "8496");
+  //OptRGBCurvesForCamMulLight("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/Lamp_with_filter", "8460");
 
+  //OptRGBCurvesForCamMulLight("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/Lamp_with_filter", "8465");
+
+  //OptRGBCurvesForCamMulLight("/home/frol/PROG/HydraRepos/rendervsphoto/Tests/LED_panel", "8496");
+
+  testGaussians();
 
   return 0;
 }
